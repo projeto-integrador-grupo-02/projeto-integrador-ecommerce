f58106d86f3c6932e0f6264291bcfe04c0b5a4a9
Jaqueline Copello <jaque.canzi@gmail.com>
2022-11-22 19:20:19 +0000

    swiper comj erro, why, lord??

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+/*   SWIPER   */
+
+var swiper = new Swiper("#hero", {
+    loop: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    }
+});
+
+let taAi = 'To'
