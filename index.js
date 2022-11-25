ddc8b4e6723589f7a3bfa86611ea9ecc3a4e3bab
Thiago Nunes <thiago.nuneswlm@hotmail.com>
2022-11-25 11:43:27 +0000

    Criado Nav e icons header

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const path = require('path')
 const servidor = express()
 
 //Definir a public como pasta de arquivos estáticos
-servidor.use(express.static("public"))
+servidor.use(express.static(path.join(__dirname, 'public'))) 
 
 //Definir as rotas
 servidor.get(
     '/home',   // endereço ou caminho a receber requisição
     (req,res) =>{          // função a ser executada
-      return res.sendFile(__dirname + '/public/html/home-dois.html')
-    }  
+      return res.sendFile(__dirname + '/views/home-dois.html')
+    }
 )
 
 // Fazer o servidor rodar
