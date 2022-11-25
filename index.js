cac14e6e27c41f01ada58f45147b845e7af4ee6a
BabiFreire <91224512+BabiFreire@users.noreply.github.com>
2022-11-25 12:32:15 -0300

    Organizando estrutura de pastas

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 // Importar o express
 const express = require('express')
 const path = require('path')
-
 // Criar o servidor
 const servidor = express()
-
 //Definir a public como pasta de arquivos estáticos
-servidor.use(express.static("public"))
-
+servidor.use(express.static(path.join(__dirname, 'public'))) 
 //Definir as rotas
-servidor.get(
-    '/home',   // endereço ou caminho a receber requisição
-    (req,res) =>{          // função a ser executada
-      return res.sendFile(__dirname + '/public/html/home-dois.html')
-    }  
-)
-
+servidor.get(    
+  '/home',   // endereço ou caminho a receber requisição    
+  (req,res) =>{          // função a ser executada      
+    return res.sendFile(__dirname + '/views/home-dois.html')    
+  })
 // Fazer o servidor rodar
-
 servidor.listen(3002)
