27e88d9b01e407dd25a40cfc2ef22fa01450a763
Jaqueline Copello <jaque.canzi@gmail.com>
2023-02-10 15:01:25 +0000

    socorro?

---
+++ public/js/admin-products.js
@@ -0,0 +1,9 @@
+const itens = document.querySelectorAll('.page-item')
+const itensMenu = document.querySelectorAll('i')
+
+itensMenu.addEventListener('click', () => {
+    console.log(funciona);
+})
+
+console.log('funciona')
+console.log(itens);
