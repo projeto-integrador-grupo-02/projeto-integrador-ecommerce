37df18f3ccfb22a70fadccbdef21f403fab8689e
Eric Azevedo <eric.azevedo9@gmail.com>
2023-02-14 21:05:19 -0300

    Alteração da rota da página do produto

--- router.js
+++ router.js
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/', PagesController.showIndex);
 router.get('/edituser', PagesController.showUser);
 router.get('/products', PagesController.showProducts)
+router.get('/produto', PagesController.showProduto)
 router.get('/cadastro', PagesController.registerUser)
 router.get('/checkout', PagesController.checkoutUser)
 router.post('/checkout/sucess', PagesController.checkoutSucess)
