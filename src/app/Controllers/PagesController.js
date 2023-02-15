e90111cc3da0842540d0f66e8db79f47a613dc1b
Eric Azevedo <eric.azevedo9@gmail.com>
2023-02-14 21:33:51 -0300

    Criada a rota para a página do carrinho

--- src/app/Controllers/PagesController.js
+++ src/app/Controllers/PagesController.js
@@ -5,6 +5,7 @@ const PagesController = {
     showUser: (req, res) => res.render('edituser.ejs'),
     showProducts: (req, res) => res.render('products.ejs'),
     showProduto: (req, res) => res.render('produto.ejs'),
+    showCarrinho: (req, res) => res.render('carrinho.ejs'),
     registerUser: (req, res) => res.render('cadastro.ejs'),
     checkoutUser: (req,res) => res.render('checkout.ejs'),
     checkoutSucess: (req,res) => res.render('checkoutSucess.ejs')
