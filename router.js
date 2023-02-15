207a2b85245a410bab9c5028dc36405735089ef8
Eric Azevedo <eric.azevedo9@gmail.com>
2023-02-14 21:34:06 -0300

    Criada a rota para a página do carrinho

--- router.js
+++ router.js
@@ -8,6 +8,7 @@ router.get('/', PagesController.showIndex);
 router.get('/edituser', PagesController.showUser);
 router.get('/products', PagesController.showProducts)
 router.get('/produto', PagesController.showProduto)
+router.get('/carrinho', PagesController.showCarrinho)
 router.get('/cadastro', PagesController.registerUser)
 router.get('/checkout', PagesController.checkoutUser)
 router.post('/checkout/sucess', PagesController.checkoutSucess)
