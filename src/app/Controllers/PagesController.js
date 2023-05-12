e8051fc5a9d758dd9562c622ec0ec5f075c7370a
Jaqueline Copello <jaque.canzi@gmail.com>
2023-05-12 22:41:08 +0100

    Home & detalhe do produto ligados ao BD

--- src/app/Controllers/PagesController.js
+++ src/app/Controllers/PagesController.js
@@ -1,49 +1,84 @@
 const path = require('path');
-const produtos = require('../../../databases/products.json');
+const {Produtos} = require('../../../databases/models')
 const session = require('express-session');
+const ProdutosServices = require('../../../services/ProdutosServices');
 
 
 const PagesController = {
-    showIndex: (req, res) => {res.render('home.ejs',{produtos})},
+    showIndex: async (req, res) => {
+        try {
+            const page = parseInt(req.query.page) || 1;
+            const perPage = 5;
+            const currentPage = parseInt(page);
+        
+            const { produtosPaginados, totalPages } = await ProdutosServices.listProducts(
+              currentPage,
+              perPage
+            );
+        
+            res.render("home.ejs", {
+              produtos: produtosPaginados,
+              totalPages,
+              currentPage,
+            });
+          } catch (error) {
+            console.log(error);
+            res.status(500).send("Ocorreu um erro ao carregar a lista de produtos.");
+          }
+        },
     showUser: (req, res) => res.render('edituser.ejs'),
     showProducts: (req, res) => res.render('products.ejs'),
-    showProduto: (req, res) => {
+    showProduto: async (req, res) => {
         const id = req.params.id
-        const produto  = produtos.find(p => p.id== id);
+        let produto = await ProdutosServices.loadProduct(id);
         res.render('produto.ejs',{produto})
     },
-    showCarrinho: (req, res) => {
-        let carrinho = req.session.carrinho
-        if (carrinho != undefined){
-
-            carrinho = carrinho.map(item => {
-                const produto = produtos.find(p => p.id == item.id);
-                    item.image = produto.image;
-                    item.name = produto.name;
-                    item.price = produto.price;
-                    item.description = produto.description;
-                    item.subtotal = Number(item.qtd) * Number(item.price);
+    showCarrinho: async (req, res) => {
+        let carrinho = req.session.carrinho;
+        if (carrinho != undefined && carrinho.length > 0) {
+          try {
+            carrinho = await Promise.all(
+              carrinho.map(async (item) => {
+                const produto = await ProdutosServices.loadProduct(item.id);
+                item.image = produto.imagem;
+                item.name = produto.nome_produto ;
+                item.price = produto.preco;
+                item.description = produto.descricao;
+                item.subtotal = Number(item.qtd) * Number(item.price);
                 return item;
-             })
-        } else{
-            carrinho = []
+              })
+            );
+          } catch (error) {
+            console.error(error);
+            carrinho = [];
+          }
+        } else {
+          carrinho = [];
         }
-        res.render('carrinho.ejs', {carrinho})
-    },
-    addCarrinho: (req, res) => {
-       if (req.session.carrinho == undefined){
-
-        req.session.carrinho = []
-       }
-       let idProduto = req.body.idProduto;
-       const produto = req.session.carrinho.find(item => item.id==idProduto)
-       if (produto == undefined){
-           req.session.carrinho.push({id: req.body.idProduto, qtd: 1})
-       } else{
-        produto.qtd++
-       }
-        res.redirect("/carrinho")
-    },
+        res.render('carrinho.ejs', { carrinho });
+      }
+      
+      ,
+      
+      addCarrinho: async (req, res) => {
+        if (req.session.carrinho == undefined) {
+          req.session.carrinho = [];
+        }
+        let idProduto = req.body.idProduto;
+        try {
+          const produto = await loadProduct(idProduto);
+          const itemIndex = req.session.carrinho.findIndex((item) => item.id === idProduto);
+          if (itemIndex === -1) {
+            req.session.carrinho.push({ id: idProduto, qtd: 1 });
+          } else {
+            req.session.carrinho[itemIndex].qtd++;
+          }
+          res.redirect('/carrinho');
+        } catch (error) {
+          console.error(error);
+          res.redirect('/carrinho');
+        }
+      },
 
     incrementarCarrinho: (req, res) => {
         let carrinho = req.session.carrinho;
